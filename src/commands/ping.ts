ae008d36910ebac729fe9744d22554d993de525f
dalmatheo <dalmatheo.comptes@gmail.com>
2022-09-27 15:03:17 +0200

    Base of a command handler and explanations.

---
+++ src/commands/ping.ts
@@ -0,0 +1,10 @@
+import {Client, Message} from "discord.js";
+
+//Export the name of the command to make the index.ts able to use it
+exports.name = "ping";
+
+//Exporting the code and say that client is a Client object, same for the message and the args.
+exports.run = (client:Client, message:Message, args:string[]) => {
+    //Sending pong into the channel of the command.
+    message.channel.send("pong!").catch(console.error)
+}
